6b9e756477c034302366a851eb0a9942e17f237e
Ranjeeteinfo <ranjeeteinfo@gmail.com>
2025-02-21 12:54:13 +0530

    Updated feature

--- MyPortfolio/settings.py
+++ MyPortfolio/settings.py
@@ -149,9 +149,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST ='smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ranjitcod@gmail.com'  # Your email address
-EMAIL_HOST_PASSWORD = 'joyr tnzp ucab jdvl'      # Your email password or app-specific password 
